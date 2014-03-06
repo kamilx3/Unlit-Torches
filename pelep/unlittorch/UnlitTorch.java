36e55349df60812f6916f6852934ed0090b615ea
pelep <pelep_ang@yahoo.com>
2014-03-06 15:34:46 +0800

    follow new versioning system

--- pelep/unlittorch/UnlitTorch.java
+++ pelep/unlittorch/UnlitTorch.java
@@ -29,7 +29,7 @@ public class UnlitTorch
 {
     public static final String MOD_NAME = "Unlit Torches";
     public static final String MOD_ID = "UnlitTorch";
-    public static final String MOD_VERSION = "2.0.30";
+    public static final String MOD_VERSION = "2.0.0";
     public static final String MOD_MCVERSION = "1.6.4";
     public static final String MOD_DEPENDENCIES = "required-after:Forge@[9.11.1.965,);required-after:PCL@[1.3.4,);before:Bushwhacker";
     public static final String MOD_CHANNEL = MOD_ID;
